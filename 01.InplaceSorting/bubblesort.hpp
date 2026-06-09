a26690b453cd55668785d17eb15c6bca764f3d54
FreeOnMinute <yarkinmakar2008@yandex.ru>
2026-06-09 09:30:18 +0300

    1 task fix

--- 01.InplaceSorting/bubblesort.hpp
+++
@@ -1,3 +0,0 @@
-#include <iostream>
-
-void bubbleSort(int m[], int size);
--- 01.InplaceSorting/qsort.hpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-
-int temp(int m[], int left, int right);
-void quickSort(int m[], int left, int right);
